6ceb984220a35a77f86e974143cc94b37c91cd54
engsr6982 <109733049+engsr6982@users.noreply.github.com>
2024-04-21 09:01:35 +0800

    fixed [Frature] Chunk-Manager #4

--- src/Permission/Permission.h
+++ src/Permission/Permission.h
@@ -8,30 +8,30 @@
 namespace tls::perms {
 
 enum Permission {
-    Unknown,                  // 未知
-    indexForm,                // 首页
-    KickPlayer,               // 踢出玩家
-    KillPlayer,               // 杀死玩家
-    ChangeWeather,            // 更改天气
-    ChangeTime,               // 更改时间
-    BroadCastMessage,         // 广播消息
-    MotdManagement,           // MOTD管理
-    SetServerMaxPlayer,       // 设置服务器最大玩家数
-    Teleport,                 // 传送
-    CleanDropItem,            // 清理掉落物品
-    ChangeGameMode,           // 更改游戏模式
-    ChangeWorldRule,          // 更改世界规则
-    GetBlockOrItem,           // 获取方块或物品
-    Terminal,                 // 终端
-    SendMessageToPlayer,      // 发送消息给玩家
-    CrashPlayerClient,        // 崩溃客户端
-    UsePlayerIdentitySay,     // 使用玩家身份说话
-    UsePlayerIdentityExecute, // 使用玩家身份执行命令
-    BanGui,                   // 封禁GUI
-    PlayerInfo,               // 玩家信息
-    CommandBlacklist,         // 命令黑名单
-    PotionGUI,                // 药水GUI
-    ChunkOpertion,            // 区块操作
+    Unknown                  = 0,  // 未知
+    indexForm                = 1,  // 首页
+    KickPlayer               = 2,  // 踢出玩家
+    KillPlayer               = 3,  // 杀死玩家
+    ChangeWeather            = 4,  // 更改天气
+    ChangeTime               = 5,  // 更改时间
+    BroadCastMessage         = 6,  // 广播消息
+    MotdManagement           = 7,  // MOTD管理
+    SetServerMaxPlayer       = 8,  // 设置服务器最大玩家数
+    Teleport                 = 9,  // 传送
+    CleanDropItem            = 10, // 清理掉落物品
+    ChangeGameMode           = 11, // 更改游戏模式
+    ChangeWorldRule          = 12, // 更改世界规则
+    GetBlockOrItem           = 13, // 获取方块或物品
+    Terminal                 = 14, // 终端
+    SendMessageToPlayer      = 15, // 发送消息给玩家
+    CrashPlayerClient        = 16, // 崩溃客户端
+    UsePlayerIdentitySay     = 17, // 使用玩家身份说话
+    UsePlayerIdentityExecute = 18, // 使用玩家身份执行命令
+    BanGui                   = 19, // 封禁GUI
+    PlayerInfo               = 20, // 玩家信息
+    CommandBlacklist         = 21, // 命令黑名单
+    PotionGUI                = 22, // 药水GUI
+    ChunkOpertion            = 23, // 区块操作
 };
 
 void initPermission();
