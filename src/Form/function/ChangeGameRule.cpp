fa9e4f445f0cf7e387f8ecaa55186be846a8b6bf
engsr6982 <109733049+engsr6982@users.noreply.github.com>
2024-04-22 18:07:22 +0800

    try fixed changeGameRule

--- src/Form/function/ChangeGameRule.cpp
+++ src/Form/function/ChangeGameRule.cpp
@@ -1,11 +1,16 @@
 #include "File/Config.h"
 #include "include_all.h"
 #include "ll/api/service/Bedrock.h"
+#include "mc/network/packet/GameRulesChangedPacket.h"
 #include "mc/world/level/storage/GameRule.h"
 #include "mc/world/level/storage/GameRuleId.h"
 #include "mc/world/level/storage/GameRules.h"
+#include <charconv>
+#include <cstdint>
 #include <filesystem>
+#include <string>
 #include <unordered_map>
+#include <variant>
 
 
 namespace tls::form {
@@ -50,6 +55,25 @@ string getTr(string rule) {
         logger.error("Unknown error in GameRule translation!"_tr());
         return rule;
     }
+}
+
+string joinErrorParameters(std::vector<string> mErrorParameters) {
+    if (mErrorParameters.empty()) return "";
+
+    return std::accumulate(
+        std::next(mErrorParameters.begin()),
+        mErrorParameters.end(),
+        mErrorParameters[0],
+        [](const string& a, const string& b) -> string { return a + "\n" + b; }
+    );
+}
+
+bool isFloat(const std::string& str) {
+    float value    = 0.0f;
+    auto [ptr, ec] = std::from_chars(str.data(), str.data() + str.size(), value);
+
+    // 检查转换是否成功，并且整个字符串都被解析
+    return ec == std::errc() && ptr == str.data() + str.size();
 }
 
 void changeGameRule(Player& player) {
@@ -99,20 +123,76 @@ void changeGameRule(Player& player) {
                     logger.warn("Unparsable game rules: {}"_tr(rule.getName()));
                 }
             } catch (...) {
-                logger.error("Failed to build game rule form"_tr());
+                logger.error("Build form fails with error location for loop"_tr());
             }
         }
 
-        fm.sendTo(player, [&](Player& pl, CustomFormResult const& dt, FormCancelReason) {
+        fm.sendTo(player, [](Player& pl, CustomFormResult const& dt, FormCancelReason) {
+            if (!dt) return sendMsg(pl, "form cancelled");
+            auto& logger = tls::entry::getInstance().getSelf().getLogger();
             try {
-                if (!dt) return sendMsg(pl, "form cancelled");
                 DebugFormCallBack(dt);
+
+                auto level = ll::service::getLevel();
+                if (!level) return sendMsg(pl, "Failed to get level Ptr"_tr());
+
+                auto rules = level->getGameRules();
+
+                for (auto [key, value] : *dt) {
+                    try {
+                        auto id = rules.nameToGameRuleIndex(key);
+                        if (id.value == (int)GameRules::GameRulesIndex::InvalidGameRule || rules.hasRule(id) == false) {
+                            logger.warn("Invalid game rule: {}"_tr(key));
+                            continue;
+                        }
+
+                        bool*                      pValueValidated = nullptr;
+                        bool*                      pValueChanged   = nullptr;
+                        GameRule::ValidationError* errorOutput     = nullptr;
+
+                        if (std::holds_alternative<uint64_t>(value)) {
+                            // bool
+                            int bl = std::get<uint64_t>(value);
+                            rules.setRule(
+                                id,
+                                bl == 0 ? false : true,
+                                false,
+                                pValueValidated,
+                                pValueChanged,
+                                errorOutput
+                            );
+                        } else if (std::holds_alternative<string>(value)) {
+                            // int or float
+                            string str = std::get<string>(value);
+                            if (isFloat(str)) {
+                                float fl = std::stof(str);
+                                rules.setRule(id, fl, false, pValueValidated, pValueChanged, errorOutput);
+                            } else {
+                                int in = std::stoi(str);
+                                rules.setRule(id, in, false, pValueValidated, pValueChanged, errorOutput);
+                            }
+                        } else {
+                            logger.warn("Invalid value type for game rule: {}"_tr(key));
+                        }
+                    } catch (...) {
+                        logger.error("Error setting game rule: {}"_tr(key));
+                    }
+                }
+
+                auto packet = rules.createAllGameRulesPacket();
+                ll::service::getLevel()->forEachPlayer([&](Player& p) {
+                    packet->sendTo(p);
+                    return true;
+                });
+                sendMsg(pl, "Operation completed!"_tr());
             } catch (...) {
-                logger.error("Failed to handle Game Rule Form Callback"_tr());
+                logger.error("Error executing form callback"_tr());
             }
         });
     } catch (...) {
-        logger.error("Failed to Build and Send Game Rule Form"_tr());
+        logger.error(
+            "Unknown error in function changeGameRule, ignore this error if the function is working properly"_tr()
+        );
     }
 }
 
