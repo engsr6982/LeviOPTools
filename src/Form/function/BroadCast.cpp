482adbf71f76548480c25787845c4e15d8f1e6b7
engsr6982 <109733049+engsr6982@users.noreply.github.com>
2024-04-23 11:40:01 +0800

    fixed broadCastMessage

--- src/Form/function/BroadCast.cpp
+++ src/Form/function/BroadCast.cpp
@@ -16,21 +16,22 @@ void broadCastMessage(Player& player) {
         });
     }
 
-    fm.appendInput("reason", "Kick Reason"_tr(), "string");
+    fm.appendInput("Message", "Message"_tr(), "string");
 
     fm.sendTo(player, [&](Player& pl, CustomFormResult const& dt, FormCancelReason) {
         if (!dt) return sendMsg(pl, "form cancelled"_tr());
         DebugFormCallBack(dt);
 
-        string reason = std::get<string>(dt->at("reason"));
+        string Message = std::get<string>(dt->at("Message"));
+
+        TextPacket pkt = TextPacket::createChat("Server", Message, "", "");
         for (auto const& [name, value] : *dt) {
             if (std::holds_alternative<uint64_t>(value)) {
                 auto isTrue = std::get<uint64_t>(value);
                 if (isTrue) {
                     Player* playerPtr = ll::service::getLevel()->getPlayer(name);
                     if (playerPtr) {
-                        reason += " "; // 修复传入empty字符串导致玩家断开连接但依然停留在假的世界中
-                        playerPtr->disconnect(reason);
+                        playerPtr->sendNetworkPacket(pkt); // send to all player
                     } else {
                         sendMsg(pl, "Failed to get player {} pointer"_tr(name));
                     }
